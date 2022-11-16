cb957a1a07b0bbfdd4833829931a7648649f9fa4
zhf_sy <zhf_sy@163.com>
2022-11-16 14:51:57 +0800

    fix-2

--- init/neo4j-image-pull.sh
+++ init/neo4j-image-pull.sh
@@ -6,14 +6,11 @@ IMAGE_NAME=neo4j
 VER=5.1
 
 
-# docker官方仓库（官方默认）
-DOCKER_REPO=docker.io/library
-
-
-# pull、tag并push
-docker pull  ${DOCKER_REPO}/${IMAGE_NAME}:${VER}
-docker tag   ${DOCKER_REPO}/${IMAGE_NAME}:${VER}   ${IMAGE_NAME##*/}:latest
-./../../deploy/docker-tag-push.sh  --tag ${VER}       ${IMAGE_NAME##*/}
-./../../deploy/docker-tag-push.sh                     ${IMAGE_NAME##*/}
+# pull、tag
+docker pull  ${IMAGE_NAME}:${VER}
+docker tag   ${IMAGE_NAME}:${VER}   ${IMAGE_NAME##*/}:latest
+# push
+./../deploy/docker-tag-push.sh  --tag ${VER}  ${IMAGE_NAME##*/}
+./../deploy/docker-tag-push.sh                ${IMAGE_NAME##*/}
 
 
--- init/rabbitmq-image-pull.sh
+++ init/rabbitmq-image-pull.sh
@@ -6,14 +6,10 @@ IMAGE_NAME=rabbitmq
 VER=3.11-management
 
 
-# docker官方仓库（默认，一般不用改）
-DOCKER_REPO=docker.io/library
-
-
-# pull、tag并push
-docker pull  ${DOCKER_REPO}/${IMAGE_NAME}:${VER}
-docker tag   ${DOCKER_REPO}/${IMAGE_NAME}:${VER}   ${IMAGE_NAME##*/}:latest
-./../deploy/docker-tag-push.sh  --tag ${VER}       ${IMAGE_NAME##*/}
-./../deploy/docker-tag-push.sh                     ${IMAGE_NAME##*/}
-
+# pull、tag
+docker pull  ${IMAGE_NAME}:${VER}
+docker tag   ${IMAGE_NAME}:${VER}   ${IMAGE_NAME##*/}:latest
+# push
+./../deploy/docker-tag-push.sh  --tag ${VER}  ${IMAGE_NAME##*/}
+./../deploy/docker-tag-push.sh                ${IMAGE_NAME##*/}
 
