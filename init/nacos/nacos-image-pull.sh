095570dc5d757509a5a20082addf0a4f7aaf6842
zhf_sy <zhf_sy@163.com>
2022-11-16 14:56:10 +0800

    fix-3

--- init/nacos/nacos-image-pull.sh
+++ init/nacos/nacos-image-pull.sh
@@ -10,7 +10,7 @@ VER=v2.1.2
 docker pull  ${IMAGE_NAME}:${VER}
 docker tag   ${IMAGE_NAME}:${VER}   ${IMAGE_NAME##*/}:latest
 # push
-./../deploy/docker-tag-push.sh  --tag ${VER}  ${IMAGE_NAME##*/}
-./../deploy/docker-tag-push.sh                ${IMAGE_NAME##*/}
+./../../deploy/docker-tag-push.sh  --tag ${VER}  ${IMAGE_NAME##*/}
+./../../deploy/docker-tag-push.sh                ${IMAGE_NAME##*/}
 
 
