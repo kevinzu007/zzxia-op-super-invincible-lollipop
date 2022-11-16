17b47f2238a2e7e43c47c90ed632da54b09a53ac
zhf_sy <zhf_sy@163.com>
2022-11-16 14:28:29 +0800

    fix-1

--- init/neo4j-image-pull.sh
+++ init/neo4j-image-pull.sh
@@ -13,7 +13,7 @@ DOCKER_REPO=docker.io/library
 # pull、tag并push
 docker pull  ${DOCKER_REPO}/${IMAGE_NAME}:${VER}
 docker tag   ${DOCKER_REPO}/${IMAGE_NAME}:${VER}   ${IMAGE_NAME##*/}:latest
-./../deploy/docker-tag-push.sh  --tag ${VER}       ${IMAGE_NAME##*/}
-./../deploy/docker-tag-push.sh                     ${IMAGE_NAME##*/}
+./../../deploy/docker-tag-push.sh  --tag ${VER}       ${IMAGE_NAME##*/}
+./../../deploy/docker-tag-push.sh                     ${IMAGE_NAME##*/}
 
 
