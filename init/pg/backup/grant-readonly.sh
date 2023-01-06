e7cf11660e58b587e1627c808faeb08d83254e4c
zhf_sy <zhf_sy@163.com>
2023-01-06 09:32:50 +0800

    fix:pg授权

--- init/pg/backup/grant-readonly.sh
+++ init/pg/backup/grant-readonly.sh
@@ -6,8 +6,10 @@ do
     echo "----- ${DB} -----"
 /usr/local/pgsql/bin/psql << EOF
     \c ${DB}
-    grant select on all tables in schema public to ff83df9995bc56df ;
-    alter default privileges in schema public grant select ON tables TO ff83df9995bc56df ;
+    grant select on all tables    in schema public to ff83df9995bc56df ;
+    grant select on all sequences in schema public to ff83df9995bc56df ;
+    alter default privileges in schema public grant select ON tables    TO ff83df9995bc56df ;
+    alter default privileges in schema public grant select ON sequences TO ff83df9995bc56df ;
     \q
 EOF
 done < ./pg_db.list
